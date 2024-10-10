f371604f2469e8930a28b7e416911b6f26ce3431
MADLAD3718 <scheuringjack@gmail.com>
2024-10-10 09:30:01 -0400

    Validate burn entities

--- scripts/burn.ts
+++ scripts/burn.ts
@@ -5,8 +5,7 @@ import { directionToSun } from "./sundirection";
 const overworld = world.getDimension(MinecraftDimensionTypes.overworld);
 
 function isInSunlight(entity: Entity): boolean {
-    const {dimension} = entity;
-    return dimension.getBlockFromRay(entity.getHeadLocation(), directionToSun(), {
+    return overworld.getBlockFromRay(entity.getHeadLocation(), directionToSun(), {
         includePassableBlocks: true,
         includeLiquidBlocks: true
     }) === undefined;
@@ -20,8 +19,8 @@ export function* sunlightBurn() {
     if ((world.getTimeOfDay() + 540) % 24000 >= 13082) return;
     if (getCurrentWeather() !== WeatherType.Clear) return;
     for (const entity of overworld.getEntities({families: ["burns_in_sunlight"]})) {
-        const {dimension, location} = entity;
-        if (dimension.getBlock(location)?.typeId === "minecraft:water") continue;
+        if (!entity.isValid()) continue;
+        if (overworld.getBlock(entity.location)?.typeId === "minecraft:water") continue;
         if (isOnFire(entity)) continue;
         if (hasHelmet(entity)) continue;
         if (isInSunlight(entity))
